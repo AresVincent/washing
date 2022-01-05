340d9ed1c6147a5b8666a25b0e8fb9ed62a2e1c8
AresVincent <vincentso@hkares.com>
2022-01-05 10:23:48 +0800

    add whatsapp hyperlink at payment page

--- src/components/payment/FPS.vue
+++ src/components/payment/FPS.vue
@@ -5,7 +5,7 @@
         <van-cell title="賬戶名稱" value="King's Cargo Logistics Limited" />
         <!-- <van-cell title="" value="" /> -->
         <p>您可透過網上及流動理財使用「轉數快」進行即時轉賬，或使用支援「轉數快」的儲值支付工具。</p>
-        <p>注意：轉賬後請發送圖片至Whatsapp：+852 69300097 進行驗證,否則將視為無效訂單！</p>
+        <p>注意：轉賬後請發送圖片至<a href="http://wa.me/85269300097" target="_blank">Whatsapp：+852 69300097</a> 進行驗證,否則將視為無效訂單！</p>
     </div>
 </template>
 <script>
--- src/components/result/Audit.vue
+++ src/components/result/Audit.vue
@@ -3,7 +3,7 @@
         <van-col :span="24">
             <van-icon name="checked" size="64px" color="green" />
             <p class="detail">你的訂單{{orderNo}}已成功提交！</p>
-            <p class="description">麻煩截圖將支付憑證發送至whatsapp：+852 69300097 激活該訂單!</p>
+            <p class="description">麻煩截圖將支付憑證發送至<a href="http://wa.me/85269300097" target="_blank">whatsapp：+852 69300097</a> 確認該訂單!</p>
         </van-col>
         <van-col span="20"><van-button class="btn" @click="reload">繼續落單</van-button></van-col>
         <van-col span="20"><router-link to="/history" class="btn">我的訂單</router-link></van-col>
